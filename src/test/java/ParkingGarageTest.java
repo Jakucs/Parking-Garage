a5f9b8a92c9479553cfd8490b9da334500666090
Jakucs Viktor <jakucs42@gmail.com>
2026-07-17 23:09:36 +0200

    every tests passed

--- src/test/java/ParkingGarageTest.java
+++ src/test/java/ParkingGarageTest.java
@@ -36,4 +36,21 @@ public class ParkingGarageTest {
         parkingGarage.getTotalIncomeByType(car);
         assertEquals(180, parkingGarage.getTotalIncomeByType(truck));
     }
+
+    @Test
+    void getTotalIncomeByTypeWithMoreTrucksTest() throws InvalidClassException {
+        ParkingGarage parkingGarage = new ParkingGarage("Aréna parking");
+        Car car = new Car("ASD-123", 4);
+        Car car2 = new Car("ASD-124", 2);
+        Truck truck = new Truck("AQUA-1", 5, 16000);
+        Truck truck2 = new Truck("AQUA-2", 4, 17000);
+        Truck truck3 = new Truck("AQUA-3", 9, 20000);
+        parkingGarage.addRecord(car);
+        parkingGarage.addRecord(car2);
+        parkingGarage.addRecord(truck);
+        parkingGarage.addRecord(truck2);
+        parkingGarage.addRecord(truck3);
+        parkingGarage.getTotalIncomeByType(car);
+        assertEquals(602, parkingGarage.getTotalIncomeByType(truck));
+    }
 }
